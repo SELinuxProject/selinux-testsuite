ba973f8587a42995804b287ab60156a37bf801bd
Stephen Smalley <sds@tycho.nsa.gov>
2015-09-21 08:23:19 -0700

    selinux-testsuite: mmap: add tests for hugetlb anon mappings
    Anonymous hugetlb mappings are treated as file mappings since
    they operate on an internally created hugetlb file.  Test that
    the appropriate file execute checks are applied on mmap and mprotect.
    
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>

---
+++ tests/mmap/mmap_hugetlb_anon_private.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/mman.h>
+
+#define LENGTH (2*1024*1024)
+
+int main(void)
+{
+	char *ptr;
+
+	ptr = mmap(NULL, LENGTH, PROT_READ | PROT_WRITE | PROT_EXEC,
+		   MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
+	if (ptr == MAP_FAILED) {
+		perror("mmap");
+		exit(1);
+	}
+	exit(0);
+}
