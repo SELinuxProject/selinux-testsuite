ed58039cb84307eb95dc8cc41161e320f7ad0a48
Jan Stancek <jstancek@redhat.com>
2015-11-16 10:46:26 +0100

    mmap/mprotect_heap: make sure memory is allocated from heap
    This test failed on ppc64 with 64k pagesize because memory
    allocation used mmap() instead of advancing heap.
    
    Use mallopt(M_MMAP_THRESHOLD,..) with large enough value
    to discourage use of mmap().
    
    Also set length in mprotect to pagesize, kernel silently aligns
    it to pagesize anyway.
    
    Signed-off-by: Jan Stancek <jstancek@redhat.com>
    Cc: Paul Moore <paul@paul-moore.com>
    Cc: Stephen Smalley <sds@tycho.nsa.gov>
    Signed-off-by: Paul Moore <pmoore@redhat.com>

--- tests/mmap/mprotect_heap.c
+++ tests/mmap/mprotect_heap.c
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/mman.h>
+#include <malloc.h>
+
+#ifndef DEFAULT_MMAP_THRESHOLD_MAX
+#define DEFAULT_MMAP_THRESHOLD_MAX 512*1024
+#endif
 
 int main(void)
 {
@@ -10,13 +15,19 @@ int main(void)
 	int rc;
 	int pagesize = getpagesize();
 
+	rc = mallopt(M_MMAP_THRESHOLD, DEFAULT_MMAP_THRESHOLD_MAX);
+	if (rc != 1) {
+		fprintf(stderr, "mallopt failed: %d\n", rc);
+		exit(1);
+	}
+
 	rc = posix_memalign(&ptr, pagesize, pagesize);
 	if (rc) {
 		fprintf(stderr, "posix_memalign failed: %d\n", rc);
 		exit(1);
 	}
 
-	rc = mprotect(ptr, 4096, PROT_READ | PROT_EXEC);
+	rc = mprotect(ptr, pagesize, PROT_READ | PROT_EXEC);
 	if (rc < 0) {
 		perror("mprotect");
 		exit(1);
