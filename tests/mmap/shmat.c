6fbac56ba35e372953e0165724b07ea6ca84d4cc
Stephen Smalley <sds@tycho.nsa.gov>
2016-10-27 09:19:08 -0400

    selinux-testsuite: mmap: add shmat SHM_EXEC test
    Topi Miettinen asked whether execmem should disable
    shmat(...,SHM_EXEC) and provided a test program for it.
    SELinux does check execmem in this case already via
    the security_mmap_file hook call in do_shmat(), but this
    test is useful to ensure we do not regress in this area.
    Modified the test program to test for shmget() failure since
    that is possible and would render the shmat() test moot,
    and to remove the arch-specific portion since we are only
    testing SELinux enforcement during shmat() itself, not
    the subsequent ability to execute the code and since
    the selinux-testsuite is run on multiple architectures.
    The test program is run twice by the test script, once
    in a domain that is allowed execmem and once in a domain
    that is not, in order to ensure that it succeeds
    in the former case and fails in the latter, as we do for
    all of the tests.
    
    Suggested-by: Topi Miettinen <toiwoton@gmail.com>
    Signed-off-by: Stephen Smalley <sds@tycho.nsa.gov>

---
+++ tests/mmap/shmat.c
@@ -0,0 +1,26 @@
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+	int shmid, rc = 0;
+	char *execmem;
+
+	shmid = shmget(IPC_PRIVATE, 4096, IPC_CREAT | 0777);
+	if (shmid < 0) {
+		perror("shmget");
+		exit(1);
+	}
+	execmem = shmat(shmid, 0, SHM_EXEC);
+	if (execmem == ((void *) -1)) {
+		perror("shmat SHM_EXEC");
+		rc = 1;
+	} else {
+		shmdt(execmem);
+	}
+	shmctl(shmid, IPC_RMID, 0);
+	exit(rc);
+}
