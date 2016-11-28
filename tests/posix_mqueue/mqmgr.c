ac981b50e0f9d78808ddf908d08e251f8aad3010
David Graziano <david.graziano@rockwellcollins.com>
2016-11-28 12:11:12 -0600

    SELinux policy and tests for posix mqueues
    This is a set of tests for use with the posix mqueue filesystem.
    
    The policy defines three process types:
    * test_mqmangeop_t - a process to manage the creation and deletion of the queues
    * test_mqread_t - a process with read access to queues
    * test_mqwrite_t - a process with write access to queues
    
    It also defines the following types:
    * mqop_file_t - for test files
    * mqop_exec_t - for test helper executables
    * mqop_mqbase_t - base label for newly created queues via standard type_transition
    * mqop_mqread_t - label for the read message queue
    * mqop_mqwrite_t - label for the write message queue
    * mqop_mqrw_t - label for the read/write message queue
    
    They test the creation, labeling, access (read/write) and removal of queues.
    The manage domain is used to create three queues (mqread, mqwrite, mqrw). Within the
    policy a named type_transition is defined to uniquely label each of these queues.
    The other test domains then perform  read and/or write operations on the queues
    and their success/failure is evaluated based on the policy.
    
    Note: The test checks to see if mqueue named type_transitions are supported by the
    kernel. If not, after their creation chcon is used to apply the appropriate labels.
    
    Signed-off-by: David Graziano <david.graziano@rockwellcollins.com>

---
+++ tests/posix_mqueue/mqmgr.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/file.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<linux/unistd.h>
+#include<mqueue.h>
+#include<errno.h>
+
+/*
+ * Managed the creation and distruction of a posix mqueue.
+ * The first argument is the name of the mqueue to be managed
+ * (including starting '/'). The second argument is the
+ * operation. '1' to create, '0' to remove.
+ *
+ */
+int main(int argc, char **argv)
+{
+
+	mqd_t fd;
+	struct mq_attr attr;
+
+	if( argc != 3 ) {
+		printf("usage: %s </mqueue name> <0|1> \n", argv[0]);
+		exit(2);
+	}
+
+	/* initialize the queue attributes */
+	attr.mq_flags = 0;
+	attr.mq_maxmsg = 10;
+	attr.mq_msgsize = 20;
+	attr.mq_curmsgs = 0;
+
+
+	/* unlink the queue if it exists */
+	if(argv[2][0] == '0') {
+		mq_unlink(argv[1]);
+	} else if(argv[2][0] == '1') {
+		fd = mq_open(argv[1], O_CREAT, 0664, &attr);
+		if (fd != -1){
+			mq_close(fd);
+		}else{
+			printf("mqmgr:create:errno = %d\n",errno);
+			exit(2);
+		}
+	} else {
+		perror("mqmgr:invalid option");
+		exit(2);
+	}
+
+	exit(0);
+
+}
---
+++ tests/posix_mqueue/mqsnd.c
@@ -0,0 +1,37 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <mqueue.h>
+#include <errno.h>
+#include <string.h>
+
+#define MSG "TEST MESSAGE"
+
+/*
+ * Tests the reading from a posix mqueue. The first argument is the
+ * name of the mqueue to be read (including starting '/').
+ */
+int main(int argc, char **argv)
+{
+	mqd_t fd;
+	if( argc != 2 ) {
+		printf("usage: %s </mqueue name> \n", argv[0]);
+		return 2;
+	}
+
+	fd = mq_open(argv[1], O_WRONLY|O_NONBLOCK);
+	if (fd == -1) {
+		printf("mqsnd: mq_open: errno = %d\n",errno);
+		return 2;
+	}
+
+	if ( mq_send(fd, MSG, strlen(MSG), 0)) {
+		printf("mqsnd: mq_send: errno = %d\n",errno);
+		return 2;
+	}
+
+	mq_close(fd);
+	return 0;
+}
