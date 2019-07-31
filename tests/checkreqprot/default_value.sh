ed71407c5db82fb3cece4168da57ae64b2ea454e
Ondrej Mosnacek <omosnace@redhat.com>
2019-07-31 15:26:21 +0200

    tests: simplify kernel version comparison
    Replace the clumsy hand-coded script with a dead simple one based on
    sort -V. It turns out all the users of kvercmp really just check if the
    running kernel version is greater than or equal to some given version
    (or the inverse), so let's name the new script 'kver_ge' and make it
    check exactly that (and just indicate the result via exit code).
    
    Signed-off-by: Ondrej Mosnacek <omosnace@redhat.com>

--- tests/checkreqprot/default_value.sh
+++ tests/checkreqprot/default_value.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-mydir=$(dirname $0)
-DISTRO=$($mydir/../os_detect)
-kvercmp=$mydir/../kvercmp
+mydir="$(dirname $0)"
+DISTRO="$("$mydir/../os_detect")"
 
-if [ "$($kvercmp $(uname -r) 4.4)" -ge 0 ] ||
-	[ "$DISTRO" = "RHEL7" -a "$($kvercmp $(uname -r) 3.10.0-327)" -ge 0 ]; then
+if "$mydir/../kver_ge" 4.4 ||
+	{ [ "$DISTRO" = "RHEL7" ] && "$mydir/../kver_ge" 3.10.0-327; }; then
 	grep -q 0 /sys/fs/selinux/checkreqprot 2> /dev/null
 	exit $?
 fi
